a3db9f058e54865b7c40c2c1816a5cbca89659a8
sankalpawadhiya <sankalpawadhiya07@gmail.com>
2024-04-01 13:36:29 +0530

    (config) Added configuration for JWT and Django Rest Framework
    - Added expiry time for access tokens and refresh tokens, set to 365 days.
    - Added dependencies for Django Rest Framework and Django Rest Framework Simple JWT.
    - Utilized Django Rest Framework for building REST APIs.

--- event_management/settings.py
+++ event_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # dependencies
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+#  DRF configuration
+REST_FRAMEWORK  = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+           'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+# JWT configuration
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
